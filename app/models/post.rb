9a3f9c1d8f856674d49cc154da4d0316998961a4
Joyce <joycenjoroge47@gmail.com>
2023-05-08 09:34:20 +0300

    completed lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validate :custom_validator
+
+    def custom_validator 
+        clickbait = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+        if clickbait.none? { |clickbait| clickbait.match title }
+            errors.add(:title, "the title is not sufficiently clickbait-y.")
+        end
+    end
 end
